9558d51457fc12ada0544e173c0ba33fbc5eb129
Yogita Kale <yogitasagar7557@gmail.com>
2022-12-06 18:31:45 +0530

    table1 using forEach loop

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,46 +1,107 @@
 var cl = console.log;
 
-var elem = [{
-    Position: 1,
-    Name: 'Hydrogen',
-    Weight: 1.0079,
-    Symbol: 'H',
+var city = [{
+    name: "USA",
+    population: 543278654,
+    area: 7654359,
+    capital: "Washington,D.C.",
+    language:"English",
 },
 {
-    Position: 2,
-    Name: 'Oxygen',
-    Weight: 15.9994,
-    Symbol: 'O',
+    name: "Canada",
+    population: 26654327,
+    area: 159994,
+    capital: 'Ottawa',
+    language:"English and French",
 },
 {
-    Position: 3,
-    Name: 'Neon',
-    Weight: 20.1797,
-    Symbol: 'Ne',
+    name: "Mexico",
+    population: 8765432,
+    area: 201797,
+    capital: 'Mexico City',
+    language:"Spanish",
 },
 {
-    Position: 4,
-    Name: 'Carbon',
-    Weight: 12.0107,
-    Symbol: 'C',
+    name: "France",
+    population: 457324772,
+    area: 8765497,
+    capital: 'Paris',
+    language:"French",
 },
 {
-    Position: 5,
-    Name: 'Nitrogen',
-    Weight: 14.0067,
-    Symbol: 'N',
+    name: "Germany",
+    population: 56743232,
+    area: 760397,
+    capital: 'Berlin',
+    language:"German",
 },
 ];
 
 var stdInfoContainer = document.getElementById("stdInfoContainer");
 var result = '';
-elem.forEach(function(el,i){
+city.forEach(function(el,i){
     result += "<tr>";
     result += "<td>" + (i + 1) + "</td>";
-    result += "<td>" + el.Name + "</td>";
-    result += "<td>" + el.Weight + "</td>";
-    result += "<td>" + el.Symbol + "</td>";
+    result += "<td>" + el.name + "</td>";
+    result += "<td>" + el.population + "</td>";
+    result += "<td>" + el.area + "</td>";
+    result += "<td>" + el.capital + "</td>";
+    result += "<td>" + el.language + "</td>";
     result += "</tr>";
 })
 cl(result);
 stdInfoContainer.innerHTML = result;
+
+cl("break");
+
+var mobile = [{
+    name: "Apple iPhone 12",
+    price: 999,
+    color: "Black",
+    storage: 128,
+    camera:"Dual-Lens",
+},
+{
+    name: "Samsung Galaxy S21",
+    price: 874,
+    color: "White",
+    storage: 128,
+    camera:"Triple-Lens",
+},
+{
+    name: "Google Pixel 4a",
+    price: 349,
+    color: "Black",
+    storage: 64,
+    camera:"Single-Lens",
+},
+{
+    name: "OnePlus 8T",
+    price: 799,
+    color: "Aquamarine Green",
+    storage: 128,
+    camera:"Quad-Lens",
+},
+{
+    name: "Huawei P40 Pro",
+    price: 799,
+    color: "Silver Frost",
+    storage: 256,
+    camera:"Quad-Lens",
+},
+];
+
+var InfoContainer = document.getElementById("InfoContainer");
+var result = '';
+mobile.forEach(function(el,i){
+    result += "<tr>";
+    result += "<td>" + (i + 1) + "</td>";
+    result += "<td>" + el.name + "</td>";
+    result += "<td>" + el.price + "</td>";
+    result += "<td>" + el.color + "</td>";
+    result += "<td>" + el.storage + "</td>";
+    result += "<td>" + el.camera + "</td>";
+    result += "</tr>";
+})
+cl(result);
+InfoContainer.innerHTML = result;
